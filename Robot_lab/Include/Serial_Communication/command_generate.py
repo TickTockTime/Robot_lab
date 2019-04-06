f9e8c9be5a66acb7b6c2c77eea7a7449179e6a11
TickTockTime <3160102438@zju.edu.cn>
2019-04-06 18:29:48 +0800

    finish serial function

--- Robot_lab/Include/Serial_Communication/command_generate.py
+++ Robot_lab/Include/Serial_Communication/command_generate.py
@@ -3,3 +3,47 @@
 # @Time  : 4/4/2019 3:54 PM
 # @Author: Yao ChengTao
 # @File  : command_generate.py
+
+import math
+
+def command_generate(robot_id, vx, vy, w):
+
+    cmd = [0xff, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
+           0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+           0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00]
+
+    if robot_id >= 8:
+        cmd[1] = 0x01 << (robot_id - 8)
+    else:
+        cmd[2] = 0x01 << robot_id
+
+
+    if vx < 0:
+        cmd[4] = cmd[4] | (0x20)
+    cmd[4] = cmd[4] | ((abs(vx) & 0x1f0) >> 4)
+    cmd[5] = cmd[5] | ((abs(vx) & 0x0f) << 4)
+
+    if vy < 0:
+        cmd[5] = cmd[5] | (0x08)
+    cmd[5] = cmd[5] | ((abs(vy) & 0x1c0) >> 6)
+    cmd[6] = cmd[6] | ((abs(vy) & 0x3f) << 2)
+
+    if w < 0:
+        cmd[6] = cmd[6] | (0x02)
+    cmd[6] = cmd[6] | ((abs(w) & 0x100) >> 8)
+    cmd[7] = cmd[7] | (abs(w) & 0x0ff)
+    command = [hex(i) for i in cmd]
+    print(command)
+    return cmd
+
+def command_raw(robot_id, v, w, orientation):
+    # v = int(v)
+    cmd = command_generate(robot_id, int(v*math.cos(orientation)), int(v*math.sin(orientation)), int(40*w))
+    return cmd
+
+def main():
+    cmd = command_raw(1,1,0,1)
+    print(cmd)
+
+if __name__=="__main__":
+    main()
--- Robot_lab/Include/Serial_Communication/serial_send.py
+++ Robot_lab/Include/Serial_Communication/serial_send.py
@@ -5,7 +5,8 @@
 # @File  : serial_send.py
 
 import serial
-import time
+import math
+import Include.Serial_Communication.command_generate as cmd
 
 class ser_command(object):
     """
@@ -24,34 +25,45 @@ class ser_command(object):
                            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                            0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x85]
 
+    def send_cmd(self, cmd):
+        self.port.write(cmd)
+        msg = self.port.readall()
+        msg = self.msg_to_hex(msg)
+        print(msg)
+
+
     def msg_to_hex(self, string):
         msgs = []
         for s in string:
             msgs.append(hex(s))
-        print(msgs)
         return msgs
 
     def send_ready_cmd(self):
         self.port.write(self.start_cmd1)
         msg = self.port.readall()
         msg = self.msg_to_hex(msg)
+        print(msg)
         # time.sleep(1)
         self.port.write(self.start_cmd2)
         msg = self.port.readall()
         msg = self.msg_to_hex(msg)
+        print(msg)
         # if ( self.receive_msgs() == True ):
         #    self.port.write(self.start_cmd2)
         # msg = self.receive_msgs()
 
 def test():
-    go_cmd = [0xff, 0x00, 0x01, 0x01, 0x00, 0xa0, 0x00, 0x00,
+    """
+    go_cmd = [0xff, 0x00, 0x02, 0x01, 0x00, 0xa0, 0x00, 0x00,
               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
               0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00]
+    """
+    go_cmd = cmd.command_raw(1,10,0, -180*math.pi/180)
 
     robo_cmd = ser_command(com = 'COM3', baud = 115200)
     robo_cmd.send_ready_cmd()
     while True:
-        robo_cmd.port.write(go_cmd)
+        robo_cmd.send_cmd(go_cmd)
 
 if __name__=="__main__":
     test()
